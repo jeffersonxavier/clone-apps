2b65cd4af9918b87fbd44ae71ede16ea1fec4bdc
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2020-04-02 14:35:44 -0300

    creating instagram clone project

---
+++ instagram-clone/src/screens/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Main = () => (
+  <View style={styles.container}>
+    <Text>Main...</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Main;
