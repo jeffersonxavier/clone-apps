3a600cb5a181750cd80029be9b6f6e4437aa237f
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2020-04-03 09:52:14 -0300

    text input to search places

--- uber-clone/src/components/Map.js
+++ uber-clone/src/components/Map.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import MapView from 'react-native-maps';
+import Search from './Search';
 
 const Map = () => {
   const [ currentRegion, setCurrentRegion ] = useState(null);
@@ -35,6 +36,7 @@ const Map = () => {
         showsUserLocation
         loadingEnabled
       />
+      <Search />
     </View>
   );
 }
@@ -44,12 +46,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   mapStyle: {
     width: '100%',
     height: '100%',
-    marginTop: 45,
+    marginTop: Platform.OS === 'android' ? 23 : 0,
   },
 });
 
