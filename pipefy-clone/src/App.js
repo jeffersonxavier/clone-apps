40d0389bc2d0d410f1392677b78ec5e9c78d4ec2
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2020-05-06 21:16:42 -0300

    structuring app to use styled components

--- pipefy-clone/src/App.js
+++ pipefy-clone/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Pipefy</h1>
+    <>
+      <GlobalStyle/>
+      <Header/>
+    </>
   );
 }
 
